8e1ed2d99d27039559fb50fe9ae32595b3e603b9
Cosmin Maria <acosmin.maria@gmail.com>
2026-02-02 13:18:34 +0200

    started integrating fireworks

---
+++ packages/uipath_langchain_client/src/uipath_langchain_client/clients/fireworks/__init__.py
@@ -0,0 +1,4 @@
+from uipath_langchain_client.clients.fireworks.chat_models import UiPathChatFireworks
+from uipath_langchain_client.clients.fireworks.embeddings import UiPathFireworksEmbeddings
+
+__all__ = ["UiPathChatFireworks", "UiPathFireworksEmbeddings"]
